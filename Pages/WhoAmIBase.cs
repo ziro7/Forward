052bff83c169546cd2758d0f721940880d62f661
Jacob Kjærgaard <ziro776@gmail.com>
2020-03-05 14:34:34 +0100

    Add project files.

---
+++ Pages/WhoAmIBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Forward.Pages
+{
+    public class WhoAmIBase : ComponentBase
+    {
+        //public List<WorkExperience> WorkExperience { get; set; } // TODO Make it to a class with the relevant data for a workexperience.
+
+        // One of Blazors functions which will be called when the components is going to be initiallized. Stated on ComponenBase
+        protected override Task OnInitializedAsync() {
+            InitializeWorkExperience();
+            return base.OnInitializedAsync();
+        }
+
+        private void InitializeWorkExperience() {
+            //WorkExperience = new List<WorkExperience>();
+            //WorkExperience.Add(new WorkExperience("SimCorp", "Developer", "Developer in IBOR Art", new DateTime(2018, 12, 1), DateTime.Now));
+            //WorkExperience.Add(new WorkExperience("Testhust", "Tester", "Manual tester and test manager", new DateTime(2017, 5, 1), new DateTime(2018,12,1)));
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Forward
+{
+    public class Program
+    {
+        public static void Main(string[] args) {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => {
+                    // States which class should be used to handle startup.
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
