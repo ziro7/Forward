55885f41baaf82a1dcf519ca34b71f422dd07075
Jacob Kjærgaard <ziro776@gmail.com>
2020-03-10 16:01:09 +0100

    API is working

--- ForwardBackend/Startup.cs
+++ ForwardBackend/Startup.cs
@@ -28,31 +28,28 @@ namespace ForwardBackend
             // Registers that the DB Context uses a SQL Server and the connection string to the DB is the default defined in the appsettings.json file.
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddControllers();
+
             // Registering the the models
             services.AddTransient<IJobRepository, JobRepository>(); //When asking for IJobRepository a New JobRepository is returned.
             services.AddScoped<IJobRepository,JobRepository>(); // Needed to create the Entity database model
             
-            services.AddMvc(); //maybe it should be mvcCore. Enabling the mvc services
-            services.AddControllers();
+            //services.AddMvc(); //maybe it should be mvcCore. Enabling the mvc services
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
 
             // Middleware component
+            app.UseRouting();
             app.UseDeveloperExceptionPage(); // gets more information on crash - should not be in release tho
             app.UseStatusCodePages(); // adds the status code 200 etc.
             app.UseStaticFiles(); // Enables static files (search in www. files) - might not be needed in api
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
             });
+
         }
     }
 }
