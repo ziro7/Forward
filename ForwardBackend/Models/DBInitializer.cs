22b6db2692b8de99ddf43b20640f4bb32676bafa
Jacob Kjærgaard <ziro776@gmail.com>
2020-03-10 15:31:14 +0100

    API launch don't quite work as intended.

--- ForwardBackend/Models/DBInitializer.cs
+++ ForwardBackend/Models/DBInitializer.cs
@@ -11,17 +11,17 @@ namespace ForwardBackend.Models
 
             if (!context.Jobs.Any()) {
                 context.AddRange(
-                    new Job { JobId = 1, CompanyName = "SimCorp", StartDate = new DateTime(2018,12,01), EndDate = new DateTime(2020,12,01), WorkExperiences=new List<WorkExperience>() { 
-                        new WorkExperience { Id = 1, Titel = "Developer", Description = "IBOR Art", FromDate = new DateTime(2018, 12, 01), EndDate = new DateTime(2020, 12, 01) } } 
+                    new Job { CompanyName = "SimCorp", StartDate = new DateTime(2018,12,01), EndDate = new DateTime(2020,12,01), WorkExperiences=new List<WorkExperience>() { 
+                        new WorkExperience { Titel = "Developer", Description = "IBOR Art", FromDate = new DateTime(2018, 12, 01), EndDate = new DateTime(2020, 12, 01) } } 
                     },
                     new Job {
-                        JobId = 2, CompanyName = "Testhuset", StartDate = new DateTime(2017, 01, 01), EndDate = new DateTime(2018, 12, 01), WorkExperiences = new List<WorkExperience>() {
-                        new WorkExperience { Id = 2, Titel = "Tester", Description = "Landbrugsstyrelsen", FromDate = new DateTime(2018, 12, 01), EndDate = new DateTime(2020, 12, 01) },
-                        new WorkExperience { Id = 3, Titel = "Test Manager", Description = "Skandia", FromDate = new DateTime(2018, 12, 01), EndDate = new DateTime(2020, 12, 01) },
-                        new WorkExperience { Id = 4, Titel = "Tester", Description = "DGD & EnergiNet", FromDate = new DateTime(2018, 12, 01), EndDate = new DateTime(2020, 12, 01) }}
+                        CompanyName = "Testhuset", StartDate = new DateTime(2017, 01, 01), EndDate = new DateTime(2018, 12, 01), WorkExperiences = new List<WorkExperience>() {
+                        new WorkExperience { Titel = "Tester", Description = "Landbrugsstyrelsen", FromDate = new DateTime(2018, 12, 01), EndDate = new DateTime(2020, 12, 01) },
+                        new WorkExperience { Titel = "Test Manager", Description = "Skandia", FromDate = new DateTime(2018, 12, 01), EndDate = new DateTime(2020, 12, 01) },
+                        new WorkExperience { Titel = "Tester", Description = "DGD & EnergiNet", FromDate = new DateTime(2018, 12, 01), EndDate = new DateTime(2020, 12, 01) }}
                     },
-                    new Job {JobId = 3, CompanyName = "Schantz", StartDate = new DateTime(2015, 05, 01), EndDate = new DateTime(2017, 01, 01), WorkExperiences = new List<WorkExperience>() {
-                        new WorkExperience { Id = 5, Titel = "Tester", Description = "Landbrugsstyrelsen", FromDate = new DateTime(2018, 12, 01), EndDate = new DateTime(2020, 12, 01) }}
+                    new Job {CompanyName = "Schantz", StartDate = new DateTime(2015, 05, 01), EndDate = new DateTime(2017, 01, 01), WorkExperiences = new List<WorkExperience>() {
+                        new WorkExperience { Titel = "Tester", Description = "Landbrugsstyrelsen", FromDate = new DateTime(2018, 12, 01), EndDate = new DateTime(2020, 12, 01) }}
                     }
                 );
 
