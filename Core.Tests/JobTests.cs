5ccd7a982cc3e45f4e418be1d6a6203332b2a9da
Jacob Kjærgaard <ziro776@gmail.com>
2020-03-22 08:16:09 +0100

    Adding Unit tests for core

--- Core.Tests/JobTests.cs
+++ Core.Tests/JobTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Core.Tests
@@ -6,13 +7,75 @@ namespace Core.Tests
     public class JobTests
     {
         [Fact]
-        public void IsDatesValid_InvalidDates_ReturnFalse() {
+        public void IsValid_ValidInout_ReturnTrue() {
             // Arrange
-
+            var job = new Job() {
+                CompanyName = "Company1",
+                StartDate = new DateTime(2018, 12, 1),
+                EndDate = new DateTime(2020, 12, 1),
+                WorkExperiences = new List<WorkExperience>() { new WorkExperience() {
+                    Titel="Developer",
+                    FromDate=new DateTime(2018,12,1),
+                    EndDate=new DateTime(2020,12,1)
+                } }
+            };
             // Act
-
+            var result = job.IsValid();
             // Assert
+            Assert.True(result);
+        }
 
+        [Fact]
+        public void IsValid_InvalidStartDates_ReturnFalse() {
+            // Arrange
+            var job = new Job() {
+                CompanyName = "Company1",
+                StartDate = new DateTime(2018, 12, 1),
+                EndDate = new DateTime(2020, 12, 1),
+                WorkExperiences = new List<WorkExperience>() { new WorkExperience() { 
+                    Titel="Developer",
+                    FromDate=new DateTime(2001,12,1),
+                    EndDate=new DateTime(2020,12,1)
+                } }
+            };
+            // Act
+            var result = job.IsValid();
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValid_InvalidEndDates_ReturnFalse() {
+            // Arrange
+            var job = new Job() {
+                CompanyName = "Company1",
+                StartDate = new DateTime(2018, 12, 1),
+                EndDate = new DateTime(2020, 12, 1),
+                WorkExperiences = new List<WorkExperience>() { new WorkExperience() {
+                    Titel="Developer",
+                    FromDate=new DateTime(2018,12,1),
+                    EndDate=new DateTime(2021,12,1)
+                } }
+            };
+            // Act
+            var result = job.IsValid();
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValid_NewJob_ReturnTrue() {
+            // Arrange
+            var job = new Job() {
+                CompanyName = "Company1",
+                StartDate = new DateTime(2018, 12, 1),
+                EndDate = new DateTime(2020, 12, 1),
+                WorkExperiences = new List<WorkExperience>()
+        };
+            // Act
+            var result = job.IsValid();
+            // Assert
+            Assert.True(result);
         }
     }
 }
