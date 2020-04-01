289689b5f2d60c14069592128db8ee26776a6468
Jacob Kjærgaard <JBKD@simcorp.com>
2020-04-01 21:35:26 +0200

    adding model validation

--- Core/Job.cs
+++ Core/Job.cs
@@ -8,12 +8,13 @@ using Core.Validation;
 
 namespace Core
 {
-    public class Job
+    public class Job : IValidatableObject
     {
         public int JobId { get; set; }
         [Required]
         [StringLength(100, ErrorMessage ="Company name is too long")]
         public string CompanyName { get; set; }
+        [Required]
         [StartDateValidator(myBirthday = "12-10-1982")]
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
@@ -24,6 +25,23 @@ namespace Core
             return IsDatesValid();
         }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) {
+            var jobErrors = new List<ValidationResult>();
+
+            if (WorkExperiences != null && WorkExperiences.Count > 0) {
+                foreach (var experience in WorkExperiences) {
+                    if (experience.FromDate < StartDate) {
+                        jobErrors.Add(new ValidationResult("The start date of the job function can't be before the start date of the job.", new[] { nameof(experience.FromDate) }));
+                    }
+                    if (experience.EndDate > EndDate) {
+                        jobErrors.Add(new ValidationResult("The end date of the job function can't be before the end date of the job.", new[] { nameof(experience.EndDate) }));
+                    }
+                }
+            }
+            return jobErrors;
+        }
+
+        // Deprecated - TODO change unit test to the new Validation reslt.
         private bool IsDatesValid() {
             if (WorkExperiences != null && WorkExperiences.Count>0) {
                 foreach (var experience in WorkExperiences) {
